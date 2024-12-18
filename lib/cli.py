4bd3e9cca58e3805cd9dd7e5b24b624e26af9515
Marion <wekesamarion100@gmail.com>
2024-12-19 00:03:47 +0300

    created main_menu function

--- lib/cli.py
+++ lib/cli.py
@@ -1,3 +1,13 @@
 from models.game import Game
 from models.developer import Developer
 from database import get_connection
+
+def main_menu():
+    print("\n=== Game Database CLI ===")
+    print("1. Manage Developers")
+    print("2. Manage Games")
+    print("3. Library Menu")
+    print("4. Search Menu")
+    print("5. Account Menu")
+    print("6. Exit")
+    return input("Choose an option: ")
